01d23a70bf32344b618d8e89b0ce9c055eb2a272
Mutaz Alajlan <Mutaz.Alajlan@tuwaiq.edu.sa>
2021-05-08 03:25:42 +0100

    Tweet tokenizer, first version.

---
+++ TwitterTokenizer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TwitterTokenizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tweet1 = @"Real Madrid, @Juventus and @Barcelona are threatening to extract #damages from their former partners in a doomed #European_SuperLeague. Via 
+@tariqpanja:";
+            GetTweetMatches(tweet1, '@');
+            GetTweetMatches(tweet1, '#');
+        }
+        public static void GetTweetMatches(string tweet, char chr)
+        {
+            string txt = "";
+            int textCounter = 0;
+
+            for (int i = 0; i < tweet.Length; i++)
+            {
+                if (tweet[i] == chr)
+                {
+                    txt += tweet[i];
+                    i++;
+                    if (Char.IsLetterOrDigit(tweet[i]) || tweet[i] == '_')
+                    {
+                        while (i < tweet.Length && (Char.IsLetterOrDigit(tweet[i]) || tweet[i] == '_'))
+                        {
+                            if (i < tweet.Length - 1)
+                            {
+                                txt += tweet[i];
+                                i++;
+                            }
+                            else
+                            {
+                                txt += tweet[i];
+                                break;
+                            }
+                        }
+                        i--;
+
+                        if (txt.Length > 0)
+                        {
+                            textCounter++;
+                        }
+                        Console.WriteLine(txt);
+                        txt = "";
+                    }
+                }
+                
+            }
+            Console.WriteLine("The number of match(es): " + textCounter);
+        }
+    }
+
+}
+
